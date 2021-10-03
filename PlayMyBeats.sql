c4e8a0c65de7e02c12c91018e5e6a247729b968e
jeff-dillon <dillonjg@gmail.com>
2021-10-03 18:32:35 -0400

    added test cases to PlayMyBeats.sql

--- PlayMyBeats.sql
+++ PlayMyBeats.sql
@@ -1,53 +1,32 @@
-/* PlayMyBeats.sql
- * Author: Jeff Dillon
- * Date Created: 9/30/2021
- * Description: DDL and DML for PlayMyBeats app
- *
+/*
+    Play My Beats Sample Commands
 */
 
--- Create a new table called '[Bands]' in schema '[dbo]'
--- Drop the table if it already exists
-IF OBJECT_ID('[dbo].[Bands]', 'U') IS NOT NULL
-DROP TABLE [dbo].[Bands]
-GO
--- Create the table in the specified schema
-CREATE TABLE [dbo].[Bands]
-(
-    [BandId] INT NOT NULL IDENTITY PRIMARY KEY, -- Primary Key column
-    [BandName] NVARCHAR(255) NOT NULL
-);
+EXECUTE ReadAlbums
 GO
 
-INSERT INTO Bands VALUES ('Beastie Boys');
-INSERT INTO Bands VALUES ('American Aquarium');
-INSERT INTO Bands VALUES ('A Tribe Called Quest');
-INSERT INTO Bands VALUES ('Greateful Dead');
-INSERT INTO Bands VALUES ('Joan Armatrading');
-INSERT INTO Bands VALUES ('Taylor Swift');
+
+EXECUTE ReadAlbums @BandName = "Neil Young"
 GO
 
-SELECT * FROM Bands
+EXECUTE ReadAlbums @FriendName = "Shawn"
 GO
 
--- Create a new table called '[Albums]' in schema '[dbo]'
--- Drop the table if it already exists
-IF OBJECT_ID('[dbo].[Albums]', 'U') IS NOT NULL
-DROP TABLE [dbo].[Albums]
-GO
--- Create the table in the specified schema
-CREATE TABLE [dbo].[Albums]
-(
-    [AlbumId] INT NOT NULL IDENTITY PRIMARY KEY, -- Primary Key column
-    [BandId] INT NOT NULL,
-    [Genre] NVARCHAR(50) NOT NULL,
-    [Condition] NVARCHAR(50) NOT NULL,
-    [AlbumName] NVARCHAR(255),
-    FOREIGN KEY(BandId) REFERENCES Bands(BandId)
-);
+EXECUTE CreateAlbum @AlbumName = "Harvest Moon", @BandName = "Neil Young"
 GO
 
-INSERT INTO Albums VALUES (1,'HIP HOP', 'GOOD', 'Paul''s Botique');
+EXECUTE DeleteAlbum @AlbumName = "Harvest Moon"
 GO
 
-SELECT * FROM Albums
+EXECUTE UpdateAlbum @OldAlbumName = "After The Goldrush", @NewAlbumName = "After The Gold Rush"
 GO
+
+EXECUTE LoanAlbum @AlbumName = "Good News", @FriendName = "Kristie"
+GO
+
+EXECUTE ReturnAlbum @AlbumName = "Good News"
+GO
+
+
+
+
